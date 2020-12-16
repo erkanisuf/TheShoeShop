e7214903ef6a1230c125b71f512f18f506de88e7
erkanisuf <63913743+erkanisuf@users.noreply.github.com>
2020-12-16 22:20:15 +0200

    'heroku'

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const cookieParser = require("cookie-parser");
 dotenv.config();
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 app.use(cookieParser());
 var bodyParser = require("body-parser");
 app.use(
